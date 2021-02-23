071202f839ef1cb3b2961228dec09610306efaeb
sandberg-smith <sandbergjacques500@gmail.com>
2021-02-23 08:22:26 +0200

    adjust size

--- components/Groovin.jsx
+++ components/Groovin.jsx
@@ -13,7 +13,7 @@ export default function Groovin(){
                     <div className="p-0 mt-12">
                         <img src={"/logo-transparent.svg"} className={"block mx-auto"} width={80} />
                         <h3 className="text-white text-3xl text-center font-bold">Groovin</h3>
-                        <p className="text-gray-200 text-lg my-2 text-center">A fashion design crowdsourcing platform</p>
+                        <p className="text-gray-300 text-md my-2 text-center">A fashion design crowdsourcing platform</p>
                         <img src={"/img/groovin-mobile-cart-page.png"} className={"block w-full mt-20"} />
                     </div>
                     <div className="p-0"></div>
