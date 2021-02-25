d03da30a2bb77c1b2aa8be552fdf903d623c85f0
sandberg-smith <sandbergjacques500@gmail.com>
2021-02-25 14:45:37 +0200

    make menu work:

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,17 +1,21 @@
-export default function Navbar(){
+import {useState} from "react";
+
+export default function Navbar() {
+    const [open,setOpen] = useState(true)
     return (
-        <div className={"px-5 py-3 "+ styles.container}>
+        <div className={"px-5 py-3 " + styles.container}>
             <div className="grid grid-cols-2">
                 <div className="text-left">
-                    <img src={"/logo.png"} className={"inline-block"} width={80} />
+                    <img src={"/logo.png"} className={"inline-block"} width={80}/>
                     <span className="font-bold text-lg ml-2 inline-block cursor-pointer">Groovin</span>
                 </div>
-                <div className={"text-right md:hidden"}>
-                    <button className="text-white focus:outline-none text-3xl">&#9776;</button>
+                <div className={"md:hidden text-right"}>
+                    <button onClick={()=>setOpen(!open)} className="text-white focus:outline-none text-3xl">&#9776;</button>
                 </div>
-                <div className="justify-end py-auto block md:flex">
+                <div className={`justify-end col-span-2 md:col-span-1 py-auto ${open ? 'block':'hidden'}  md:flex text-right `}>
                     <div className="flex-initial px-5 py-3">
-                        <a href={"#start"} className="text-black border-b-2 border-yellow-700 py-2">&nbsp;&nbsp;Start&nbsp;&nbsp;</a>
+                        <a href={"#start"}
+                           className="text-black border-b-2 border-yellow-700 py-2">&nbsp;&nbsp;Start&nbsp;&nbsp;</a>
                     </div>
                     <div className="flex-initial px-5 py-3">
                         <a href={"#bring"} className="text-black">Sell</a>
@@ -29,9 +33,9 @@ export default function Navbar(){
 }
 
 const styles = {
-    container:{
-        backgroundColor:"transparent",
-        color:"black",
-        zIndex:1000
+    container: {
+        backgroundColor: "transparent",
+        color: "black",
+        zIndex: 1000
     }
 }
