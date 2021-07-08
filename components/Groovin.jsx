8c24c48dccfcf802e64562823613dc5c87bfcca8
Hirwa Blessing <hirwablessing7@gmail.com>
2021-07-08 22:45:45 +0200

    Finish Bring your business online section

--- components/Groovin.jsx
+++ components/Groovin.jsx
@@ -1,24 +1,89 @@
-export default function Groovin(){
-    return (
-        <div className={"w-full m-0 p-0"} id={"groovin"}>
-            <div className="bg-app py-7 px-6">
-                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-                    <div className="p-0"></div>
-                    <div className="p-0">
-                        <img src={"/img/groovin-mobile-shop-page.png"} className={"block w-full"} />
-                    </div>
-                    <div className="p-0 md:pl-4 md:mt-28">
-                        <img src={"/img/groovin-mobile-product-details.png"} className={"block w-full"} />
-                    </div>
-                    <div className="p-0 mt-12">
-                        <img src={"/logo-transparent.svg"} className={"block mx-auto"} width={80} />
-                        <h3 className="text-white text-3xl text-center font-bold">Groovin</h3>
-                        <p className="text-gray-300 text-md my-2 text-center">A fashion design crowdsourcing platform</p>
-                        <img src={"/img/groovin-mobile-cart-page.png"} className={"block w-full mt-20"} />
-                    </div>
-                    <div className="p-0"></div>
-                </div>
+import Image from "next/image";
+
+export default function Groovin() {
+  return (
+    <div
+      className={"w-full flex flex-col md:flex-row justify-evenly my-7"}
+      id={"groovin"}
+    >
+      <div className="w-96 p-10">
+        <h1 className="text-3xl font-extrabold">
+          Bring Your <br />
+          Business Online
+        </h1>
+        <div className="flex flex-col py-8">
+          <div className="flex flex-row gap-4">
+            <div className="w-40 h-12 border border-gray-200 rounded-md flex justify-center items-center">
+              <Image
+                src="/img/explore.svg"
+                alt="Become visible"
+                width={30}
+                height={30}
+              />
             </div>
+
+            <div className="flex gap-5 flex-col">
+              <div className="flex flex-col gap-2">
+                <h6 className="font-extrabold">Become Visible</h6>
+                <p className="w-full">
+                  Share your Designs, watch the world exploring and interacting
+                  with them while providing feedback.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-4 my-8">
+            <div className="w-40 h-12 border border-gray-200 rounded-md flex justify-center items-center">
+              <Image
+                src="/img/getintouch.svg"
+                alt="Become visible"
+                width={30}
+                height={30}
+              />
+            </div>
+
+            <div className="flex gap-5 flex-col">
+              <div className="flex flex-col gap-2">
+                <h6 className="font-extrabold">Become Visible</h6>
+                <p className="w-full">
+                  Share your Designs, watch the world exploring and interacting
+                  with them while providing feedback.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-row gap-4 my-4">
+            <div className="w-40 h-12 border border-gray-200 rounded-md flex justify-center items-center">
+              <Image
+                src="/img/design_relax.svg"
+                alt="Become visible"
+                width={30}
+                height={30}
+              />
+            </div>
+
+            <div className="flex gap-5 flex-col">
+              <div className="flex flex-col gap-2">
+                <h6 className="font-extrabold">Become Visible</h6>
+                <p className="w-full">
+                  Share your Designs, watch the world exploring and interacting
+                  with them while providing feedback.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="md:w-4/12 w-ful md:m-0 m-8">
+        <Image
+          src="/img/bring_business_online.svg"
+          alt="Become visible"
+          width={695}
+          height={870}
+        />
+      </div>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import Footer from "../components/Footer";
 import Groovin from "../components/Groovin";
 import Navbar from "../components/Navbar";
@@ -9,20 +9,19 @@ import Discover from "../components/Discover";
 
 export default function Home() {
   return (
-      <div className={"m-0 p-0"}>
-          <Head>
-              <title>Groovin | Home</title>
-              <link rel="icon" href="/logo.png" />
-
-          </Head>
-          <div className="content min-h-screen">
-              {/*developed by Jacques Twizeyimana*/}
-              <Welcome />
-              <Groovin/>
-              <BringYourBusinessToUs />
-              <Discover />
-          </div>
-          <Footer />
+    <div className={"m-0 p-0"}>
+      <Head>
+        <title>Groovin | Home</title>
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <div className="content min-h-screen">
+        {/*developed by Jacques Twizeyimana and Blessing Hirwa*/}
+        <Welcome />
+        <Groovin />
+        <BringYourBusinessToUs />
+        <Discover />
       </div>
-  )
+      <Footer />
+    </div>
+  );
 }
